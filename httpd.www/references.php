ba71e9f43ea644713db0c23a2e44abd5dfd9c906
Abdullah Sahin <abdullah.sahin@dwpbank.de>
2023-01-19 08:08:06 +0100

    feat: add links to references and add new ref

--- httpd.www/references.php
+++ httpd.www/references.php
@@ -14,73 +14,101 @@ if (basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME'])) {
 
         <div class="swiper-slide">
           <div class="testimonial-item">
-            <h3>SoPlan</h3>
-            <h4>Desktop App</h4>
-            <p>
-              <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-              A planing tool for students. Build with Angular & Electron as a Desktop App.
-              <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-            </p>
+            <a target="_blank" href="https://github.com/abdellaui/SoPlan">
+
+              <h3>SoPlan</h3>
+              <h4>Desktop App</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                A planing tool for students. Build with Angular & Electron as a Desktop App.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </a>
           </div>
         </div><!-- End Reference item-->
 
         <div class="swiper-slide">
           <div class="testimonial-item">
-            <h3>Routenfahrer</h3>
-            <h4>Android & iOS App</h4>
-            <p>
-              <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-              Managing your delivery routes via an app. Build with Angular & Ionic.
-              <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-            </p>
+            <a target="_blank" href="https://sahin.uk/routenfahrer/">
+
+              <h3>Routenfahrer</h3>
+              <h4>Android & iOS App</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Managing your delivery routes via an app. Build with Angular & Ionic.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </a>
           </div>
         </div><!-- End Reference item-->
 
         <div class="swiper-slide">
           <div class="testimonial-item">
-            <h3>Automobile-Residence</h3>
-            <h4>Website</h4>
-            <p>
-              <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-              Build a responsive website for exporting / importing & buying and selling used cars. Based on our custom IYS-CMS.
-              <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-            </p>
+            <a target="_blank" href="https://automobile-residence.de/">
+
+              <h3>Automobile-Residence</h3>
+              <h4>Website</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Build a responsive website for exporting / importing & buying and selling used cars. Based on our custom IYS-CMS.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </a>
           </div>
         </div><!-- End Reference item-->
 
         <div class="swiper-slide">
           <div class="testimonial-item">
-            <h3>IYS-CMS</h3>
-            <h4>Content-Management-System</h4>
-            <p>
-              <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-              A basic Content-Management-System. Build with PHP, jQuery & AdminLTE design.
-              <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-            </p>
+            <a target="_blank" href="https://github.com/abdellaui/iys-cms/">
+
+              <h3>IYS-CMS</h3>
+              <h4>Content-Management-System</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                A basic Content-Management-System. Build with PHP, jQuery & AdminLTE design.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </a>
           </div>
         </div><!-- End Reference item-->
 
         <div class="swiper-slide">
           <div class="testimonial-item">
-            <h3>Habbolar</h3>
-            <h4>Web-Game</h4>
-            <p>
-              <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-              An official fansite of habbo.com.tr and complex web-game. Build with PHP, jQuery & Boostrap 3.
-              <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-            </p>
+            <a target="_blank" href="https://habbolar.com/">
+              <h3>Habbolar</h3>
+              <h4>Web-Game</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                An official fansite of habbo.com.tr and complex web-game. Build with PHP, jQuery & Boostrap 3.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </a>
           </div>
         </div><!-- End Reference item-->
 
         <div class="swiper-slide">
           <div class="testimonial-item">
-            <h3>ROS Intel RealSense T265 & D435</h3>
-            <h4>Robotic Operation System</h4>
-            <p>
-              <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-              Managing 2 types of Intel RealSense Cameras. Build with C++, Python & ROS.
-              <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-            </p>
+            <a target="_blank" href="https://github.com/abdellaui/ros_intel_realsense_t265_d435/">
+              <h3>ROS Intel RealSense T265 & D435</h3>
+              <h4>Robotic Operation System</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Managing 2 types of Intel RealSense Cameras. Build with C++, Python & ROS.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+          </div>
+        </div><!-- End Reference item-->
+
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <a target="_blank" href="https://github.com/abdellaui/camera_construction_blender_plugin/">
+              <h3>Camera Construction Blender Plugin</h3>
+              <h4>Blender Plugin</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Creates virtual cameras inside a BIM-based construction. Build with Python & Blender v.2.79b.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
           </div>
         </div><!-- End Reference item-->
 
